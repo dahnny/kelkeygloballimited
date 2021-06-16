c811d6731433443f025943f0bcc9d14ba4537b3f
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-06-17 00:34:28 +0100

    --- Auto-Git Commit ---

--- models/user.js
+++ models/user.js
@@ -1,52 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
 
-
 const UserSchema = new Schema({
-
-
-
   email: {
     type: String,
-    lowercase : true,
-    required : true,
-    min: [6, 'Too short to be a mail'],
-    max: 25
+    lowercase: true,
+    required: true,
+    min: [6, "Too short to be a mail"],
+    max: 25,
   },
-  password :{
-    type : String,
-    required: true
+  password: {
+    type: String,
+    required: true,
   },
-  first_name : {
-    
-    type : String,
-    required: true
+  first_name: {
+    type: String,
+    required: true,
   },
- last_name : {
-    
-    type : String,
-    required: true
+  last_name: {
+    type: String,
+    required: true,
   },
-  referrals : [{
-    type:Schema.Types.ObjectId,
-    ref:'user'
-}]
-
-
-
-
-
+  referrals: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  is_admin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-UserSchema.methods.generateHash = function(password) {
+UserSchema.methods.generateHash = function (password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
-
 // checking if password is valid
-UserSchema.methods.validPassword = function(password) {
+UserSchema.methods.validPassword = function (password) {
   return bcrypt.compareSync(password, this.password);
 };
-module.exports = User = mongoose.model('user', UserSchema);
+module.exports = User = mongoose.model("user", UserSchema);
