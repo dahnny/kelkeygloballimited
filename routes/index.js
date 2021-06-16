03e30cb5de5ef35661b3ea7c1e9dd5399e0b7f5a
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-06-16 13:03:07 +0100

    --- Auto-Git Commit ---

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get("/contact", async(req, res) => {
 router.get("/listing", async(req, res) => {
     res.render("single_listing")
 })
-router.get("/listing/:slug", async(req, res) => {
+router.get("/:slug", async(req, res) => {
     res.render("single_listing")
 })
 
