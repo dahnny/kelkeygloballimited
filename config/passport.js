d07922c3435459b2a326f91782fadd3e5f58b0f5
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-06-18 13:17:00 +0100

    --- Auto-Git Commit ---

--- config/passport.js
+++ config/passport.js
@@ -38,7 +38,7 @@ module.exports = function(passport) {
     // we are using named strategies since we have one for login and one for signup
     // by default, if there was no name, it would just be called 'local'
 
-    passport.use('local-signup', new LocalStrategy({
+    passport.use('local-register', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
         usernameField : 'email',
         passwordField : 'password',
