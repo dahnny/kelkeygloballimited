336d20bf26457124d9090f9176381ee8e8f8b2a6
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-06-18 12:06:42 +0100

    --- Auto-Git Commit ---

--- routes/index.js
+++ routes/index.js
@@ -184,6 +184,24 @@ router.get("/admin-panel/properties", async(req, res) => {
 })
 
 
+router.get("/admin-panel/users", async(req, res) => {
+
+  // var post_length = await (await Post.find()).length
+  // var posts = await Post.find()
+
+  // var views =await posts.reduce((n, {views}) => n + views, 0)
+
+  // var published = await (await Post.find({status : "published"})).length
+
+  // console.log({views })
+    res.render("admin/Users",{
+      // post_length,
+      // views,
+      // published
+    })
+})
+
+
 
 
 
