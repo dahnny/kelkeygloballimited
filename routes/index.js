f265eabbcdf4d88557606da77d91dbb8cf24d31c
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-06-19 02:09:32 +0100

    --- Auto-Git Commit ---

--- routes/index.js
+++ routes/index.js
@@ -417,7 +417,8 @@ router.get("/listing/:slug", async (req, res) => {
     const rows=  Math.round(Math.abs(total_amenities/(total_amenities/3)) )
     console.log({rows})
     res.render("single_listing", {
-    property
+    property,
+    moment
     });
   }else{
     res.redirect("/listing")
