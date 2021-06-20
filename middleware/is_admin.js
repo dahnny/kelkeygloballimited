ba97e599925323ed9c51e83d8c715e72a0cef559
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-06-20 18:56:54 +0100

    --- Auto-Git Commit ---

--- middleware/is_admin.js
+++ middleware/is_admin.js
@@ -1,10 +1,9 @@
 function admin(req, res, next) {
     // if user is authenticated in the session, carry on
     console.log({req})
-    if (req.isAuthenticated()) return next();
+    if (req.user.is_admin) return next();
     // if they aren't redirect them to the home page
-    // res.redirect("/login");
-    console.log("Only Admins can perform this function")
+    res.redirect("/dashboard")
   }
 
   module.exports = admin;
