d9db6a490e81b54ed4cc2cbc2d639c080564d7cb
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-06-20 18:55:23 +0100

    --- Auto-Git Commit ---

--- middleware/loggedIn.js
+++ middleware/loggedIn.js
@@ -1,5 +1,6 @@
 function isLoggedIn(req, res, next) {
     // if user is authenticated in the session, carry on
+    console.log({req})
     if (req.isAuthenticated()) return next();
     // if they aren't redirect them to the home page
     res.redirect("/login");
